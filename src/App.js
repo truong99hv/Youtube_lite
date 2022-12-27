c58758a1d57b14488a32d241c71c97cdfdc2ecbe
Hoàng Trường <congatruong99pro@gmail.com>
2022-12-27 09:25:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { AppContext } from "./context/contextApi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext>
+      <div className="text-3xl bg-red-800 mt-6">App</div>
+    </AppContext>
   );
 }
 
